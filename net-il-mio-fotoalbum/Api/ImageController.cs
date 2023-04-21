990f6f9620cee6e4e8eecdc8f27a925c02801195
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-04-21 16:55:08 +0200

    api

---
+++ net-il-mio-fotoalbum/Api/ImageController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly AlbumContext _context;
+
+        public ImageController(AlbumContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/")]
+        public IActionResult GetImages([FromQuery] string? title)
+        {
+            var images = _context.Images!
+                .Where(i => title == null ||
+                 i.Title.ToLower()
+                .Contains(title.ToLower()))
+                .Include(i => i.Categories)
+                .ToList();
+
+            foreach (var image in images) image.Categories = null;
+
+            return Ok(images);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetImage(int? id)
+        {
+            var image = _context.Images!.FirstOrDefault(p => p.Id == id);
+
+            if (image is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(image);
+        }
+
+    }
+}
--- net-il-mio-fotoalbum/wwwroot/js/site.js
+++ net-il-mio-fotoalbum/wwwroot/js/site.js
@@ -1,4 +1,56 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+const loadImages = filter => getImages(filter).then(renderImages);
 
-// Write your JavaScript code.
+const getImages = title =>axios.get('api/image', title ? { params: { title } } : {})
+        .then(res => res.data)
+        .catch(e => console.log(e));
+
+const renderImages = images => {
+    const page = document.querySelector('#page');
+    const loader = document.querySelector('#images-loader');
+    const noImages = document.querySelector('#no-images'); 
+    const filter = document.querySelector("#filter");
+
+    if (images && images.length > 0) {
+        noImages.classList.remove('d-none');
+        page.classList.add('d-none');
+        filter.classList.add('d-none');
+    } else {
+        noImages.classList.add('d-none');
+        page.classList.remove('d-none');
+    }
+
+    loader.classList.add('d-none');
+
+    page.innerHTML = images.map(imageComponent).join('');
+};
+
+const imageComponent= image => `
+        <div class="col">
+            <div class="card">
+                <img src="${image.url}" class="card-img-top" alt="${image.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${image.title}</h5>
+                    <p class="card-text">${image.description}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        Categorie:
+                        ${image.categories.map(categoryComponent).join('')}    
+                    </li>
+                </ul>
+            </div>
+        </div>
+    `;
+
+const categoryComponent = category => `<span>${category.name}, </span>`
+
+
+const initFilter = ()  => {
+    const filter = document.querySelector("#filter");
+    filter.addEventListener("input", (e) => loadImages(e.target.value))
+};
+
+const getImage = id => axios
+    .get(`/api/image/${id}`)
+    .then(res => res.data);
